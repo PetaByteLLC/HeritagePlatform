b834d9fc085525dcf5b782f115792ff0de9a51d1
Kairat Tobokelov <kajrattobokelov2@gmail.com>
2025-01-28 15:47:20 +0900

    initialized project

---
+++ src/components/Map.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef } from 'react';
+import 'ol/ol.css';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { defaults as defaultControls } from 'ol/control';
+
+const OpenLayersMap = () => {
+  const mapRef = useRef();
+
+  useEffect(() => {
+    const map = new Map({
+      target: mapRef.current,
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+      controls: defaultControls({
+        zoom: false
+      }),
+    });
+
+    return () => map.setTarget(null);
+  }, []);
+
+  return <div ref={mapRef} className="map-container" />;
+};
+
+export default OpenLayersMap;

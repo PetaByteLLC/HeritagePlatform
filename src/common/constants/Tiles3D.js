4c7b9a701173aa01ece53d1b389f27f9ddff90ca
SharshenovJR <sharshenov.jr@gmail.com>
2025-01-30 11:18:05 +0900

    added 3D tiles

---
+++ src/common/constants/Tiles3D.js
@@ -0,0 +1,49 @@
+const layers3D = {
+  Default: {
+    name: 'satellitehybrid',
+    quality: 'middle',
+    zerolevelOffset: 1,
+  },
+  Google: {
+    name: 'satellitehybrid',
+    quality: 'middle',
+    zerolevelOffset: 1,
+  },
+  BingMap: {
+    name: 'satellitehybrid',
+    quality: 'middle',
+    zerolevelOffset: 1,
+  },
+  OpenStreetMap: {
+    name: 'terrain',
+    quality: 'middle',
+    zerolevelOffset: 1,
+  },
+  ArcMap: {
+    name: 'vectorhybrid',
+    quality: 'middle',
+    zerolevelOffset: 1,
+  },
+  MapBox: {
+    name: 'satellite',
+    quality: 'middle',
+    zerolevelOffset: 1,
+  },
+  Naver: {
+    name: 'satellite',
+    quality: 'middle',
+    zerolevelOffset: 1,
+  },
+  Daum: {
+    name: 'satellite',
+    quality: 'middle',
+    zerolevelOffset: 1,
+  },
+  SKYMap: {
+    name: '2020',
+    quality: 'middle',
+    zerolevelOffset: 1,
+  },
+};
+
+export default layers3D;

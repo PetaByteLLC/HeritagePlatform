b20edfc809a7e5b75139808ea0d5666a788deef2
Kairat Tobokelov <kajrattobokelov2@gmail.com>
2025-02-04 17:07:18 +0900

    added 2 layer search for POI

--- src/common/constants/GeoserverConfig.js
+++ src/common/constants/GeoserverConfig.js
@@ -1,6 +1,7 @@
 export const WFS_VERSION = process.env.REACT_APP_WFS_VERSION;
 export const WFS_WORKSPACE = process.env.REACT_APP_WFS_WORKSPACE;
 export const WFS_LAYER = process.env.REACT_APP_WFS_POI_LAYER;
+export const WFS_CITY_LAYER = process.env.REACT_APP_WFS_POI_CITY_LAYER;
 export const WFS_DEFAULT_NAME_FIELD = process.env.REACT_APP_DEFAULT_POI_NAME_FIELD;
 export const DEFAULT_SRS = process.env.REACT_APP_DEFAULT_SRS;
 export const POI_LAYER_NAME = process.env.REACT_APP_POI_LAYER_NAME;

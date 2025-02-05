7873ec81f03325a0fab17e4cc9254221081bf9f4
SharshenovJR <sharshenov.jr@gmail.com>
2025-02-05 12:11:04 +0900

    updated Effect2DStrategy

--- src/2D/domain/strategies/Effect2DStrategy.js
+++ src/2D/domain/strategies/Effect2DStrategy.js
@@ -1,6 +1,11 @@
 import { EffectStrategy } from "../../../common/domain/strategies/EffectStrategy";
 
 export class Effect2DStrategy extends EffectStrategy {
+    constructor(map2D) {
+        super();
+        this.map2D = map2D;
+    }
+
     handleEffectAction(effects) {
         alert('Unable to display effects in 2D mode');
         console.log('Unable to display effects in 2D mode');
